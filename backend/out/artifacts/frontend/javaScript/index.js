a0ecbcba52104e69541bdf28815bb28b806ff5d9
Jorge Cruz <jorgecruz@student.dei.uc.pt>
2025-02-16 13:27:09 +0000

    - Termina as secções de produtos no index.html; - Adiciona formato de avaliações aos produtos; - Coloca avaliações nos cartões dos produtos;

--- backend/out/artifacts/frontend/javaScript/index.js
+++ backend/out/artifacts/frontend/javaScript/index.js
@@ -1,15 +1,16 @@
 'use strict';
 
 const rootPath = 'http://localhost:8080/mariana-jorge-proj2/rest';
-
+const getAllProductsURL = `${rootPath}/products/all`;
 const addProductURL = `${rootPath}/products/add`;
 
 document.addEventListener('DOMContentLoaded', () => {
-  displayProducts();
+  displayMostRecentProducts();
+  displayMostRatedProducts();
 });
 
 async function getAllProducts() {
-  const requestURL = `${rootPath}/products/all`;
+  const requestURL = getAllProductsURL;
   const request = new Request(requestURL, {
     method: 'GET',
     headers: {
@@ -18,6 +19,7 @@ async function getAllProducts() {
   });
   const response = await fetch(request);
   const products = await response.json();
+  console.log(products);
   return products;
 }
 
@@ -35,37 +37,52 @@ async function addProduct(product) {
   return result;
 }
 
-async function displayProducts() {
-  const mainContainer = document.querySelector('.card-container');
+async function displayMostRecentProducts() {
+  const mainContainer = document.querySelector('.recent-products');
   const products = await getAllProducts();
+  const mostRecentProducts = products
+    .sort((a, b) => new Date(b.dataDePublicacao) - new Date(a.dataDePublicacao))
+    .slice(0, 3);
   mainContainer.innerHTML = '';
-  products.forEach(p => {
-    //console.log(p);
+  mostRecentProducts.forEach(p => {
     const card = createCard(p);
     mainContainer.appendChild(card);
   });
   console.log(products);
 }
 
+async function displayMostRatedProducts() {
+  const mainContainer = document.querySelector('.most-rated-products');
+  const products = await getAllProducts();
+  const mostRatedProducts = products
+    .sort((a, b) => b.rating - a.rating)
+    .slice(0, 3);
+  mainContainer.innerHTML = '';
+  mostRatedProducts.forEach(p => {
+    const card = createCard(p);
+    mainContainer.appendChild(card);
+  });
+}
+
 // Cria a card do produto
-function createCard(produto) {
+function createCard(product) {
   const card = document.createElement('div');
   card.className = 'card';
   card.innerHTML = `
-    <img src="${produto.imagem}" alt="${produto.titulo}" />
+    <img src="${product.imagem}" alt="${product.titulo}" />
     <div>
-      <h1>${produto.titulo}</h1>
-      <h4>${produto.local}</h4>
-      <h2>${produto.categoria}</h2>
-      <h3 class="rating">rating</h3>
-      <span>${parseFloat(produto.preco).toFixed(2)}€</span>
+      <h1>${product.titulo}</h1>
+      <h4>${product.local}</h4>
+      <h2>${product.categoria}</h2>
+      <h3 class="rating">${gerarRating(product.avaliacoes)}</h3>
+      <span>${parseFloat(product.preco).toFixed(2)}€</span>
       <button type="button" title="descricao">Saber mais</button>
     </div>
   `;
 
   const button = card.querySelector('button');
   button.addEventListener('click', () => {
-    window.location.href = `detalhes-produtos.html?id=${produto.id}`;
+    window.location.href = `detalhes-produtos.html?id=${product.id}`;
   });
 
   return card;
