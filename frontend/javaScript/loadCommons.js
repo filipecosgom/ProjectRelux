f960bac556b261aff0f71efc1f0655b8d5b58ca8
Jorge Cruz <jorgecruz@student.dei.uc.pt>
2025-02-17 17:48:36 +0000

    Alteração do script para módulo para importar função de carregar header e footer

--- frontend/javaScript/loadCommons.js
+++ frontend/javaScript/loadCommons.js
@@ -1,21 +1,17 @@
-"use strict";
+'use strict';
 
-document.addEventListener("DOMContentLoaded", function () {
-  loadHeaderFooter();
-});
-
-function loadHeaderFooter() {
-  fetch("common/header.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("header").innerHTML = data;
+export default function loadHeaderFooter() {
+  fetch('common/header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header').innerHTML = data;
     })
-    .catch((error) => console.error("Erro ao carregar o cabeçalho:", error));
+    .catch(error => console.error('Erro ao carregar o cabeçalho:', error));
 
-  fetch("common/footer.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("footer").innerHTML = data;
+  fetch('common/footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer').innerHTML = data;
     })
-    .catch((error) => console.error("Erro ao carregar o rodapé:", error));
+    .catch(error => console.error('Erro ao carregar o rodapé:', error));
 }
