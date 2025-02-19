5d48356e14265cda3439f438969f14b0dbe43a74
Jorge Cruz <jorgecruz@student.dei.uc.pt>
2025-02-19 18:15:59 +0000

    - refactor da função de enviar produto para o backend  (sendNewProductReq(produto))

--- frontend/javaScript/loadCommons.js
+++ frontend/javaScript/loadCommons.js
@@ -1,107 +1,123 @@
-"use strict";
+'use strict';
 
 export async function loadCommonElements() {
-  fetch("common/newProductModal.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.body.insertAdjacentHTML("beforeend", data);
+  fetch('common/newProductModal.html')
+    .then(response => response.text())
+    .then(data => {
+      document.body.insertAdjacentHTML('beforeend', data);
     })
-    .catch((error) => console.error("Erro ao carregar o modal:", error));
+    .catch(error => console.error('Erro ao carregar o modal:', error));
 
-  fetch("common/header.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("header").innerHTML = data;
+  fetch('common/header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header').innerHTML = data;
     })
     .then(() => {
       welcomeMessage();
       addModalListeners();
     })
-    .catch((error) => console.error("Erro ao carregar o cabeçalho:", error));
+    .catch(error => console.error('Erro ao carregar o cabeçalho:', error));
 
-  fetch("common/footer.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("footer").innerHTML = data;
+  fetch('common/footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer').innerHTML = data;
     })
-    .catch((error) => console.error("Erro ao carregar o rodapé:", error));
+    .catch(error => console.error('Erro ao carregar o rodapé:', error));
 }
 
 async function welcomeMessage() {
   let user = null;
-  const profilePicture = document.getElementById("profile-picture");
-  const openModalBtn = document.getElementById("openModalBtn");
-  const welcomeMessage = document.getElementById("welcome-message");
-  const logoutButton = document.getElementById("botao-logout");
-  const loginButton = document.getElementById("botao-login");
+  const profilePicture = document.getElementById('profile-picture');
+  const openModalBtn = document.getElementById('openModalBtn');
+  const welcomeMessage = document.getElementById('welcome-message');
+  const logoutButton = document.getElementById('botao-logout');
+  const loginButton = document.getElementById('botao-login');
 
   try {
-    user = JSON.parse(sessionStorage.getItem("user"));
+    user = JSON.parse(sessionStorage.getItem('user'));
   } catch (error) {
-    console.error("Erro a ler user da sessionStorage", error);
+    console.error('Erro a ler user da sessionStorage', error);
   }
 
   if (user) {
-    openModalBtn.classList.remove("hidden");
-    welcomeMessage.classList.remove("hidden");
-    logoutButton.classList.remove("hidden");
-    loginButton.classList.add("hidden");
-    profilePicture.classList.remove("hidden");
+    openModalBtn.classList.remove('hidden');
+    welcomeMessage.classList.remove('hidden');
+    logoutButton.classList.remove('hidden');
+    loginButton.classList.add('hidden');
+    profilePicture.classList.remove('hidden');
     profilePicture.src = user.imagem;
     welcomeMessage.innerHTML = `<a href="perfil-utilizador.html">Bem-vindo/a ${user.nome}</a>!`;
 
-    logoutButton.addEventListener("click", () => {
+    logoutButton.addEventListener('click', () => {
       sessionStorage.clear();
-      window.location.href = "index.html";
+      window.location.href = 'index.html';
     });
   } else {
-    openModalBtn.classList.add("hidden");
-    loginButton.classList.remove("hidden");
-    logoutButton.classList.add("hidden");
-    welcomeMessage.classList.add("hidden");
-    profilePicture.classList.add("hidden");
+    openModalBtn.classList.add('hidden');
+    loginButton.classList.remove('hidden');
+    logoutButton.classList.add('hidden');
+    welcomeMessage.classList.add('hidden');
+    profilePicture.classList.add('hidden');
   }
 }
 
 async function addModalListeners() {
-  const modal = document.getElementById("modal");
-  const btn = document.getElementById("openModalBtn");
-  const span = document.getElementsByClassName("close")[0];
-  const cancel = document.getElementById("cancelar");
-  const form = document.getElementById("form-novo-produto");
+  const modal = document.getElementById('modal');
+  const btn = document.getElementById('openModalBtn');
+  const span = document.getElementsByClassName('close')[0];
+  const cancel = document.getElementById('cancelar');
+  const form = document.getElementById('form-novo-produto');
+
+  // Criar variável para o botão submeter
+  // criar um event listener para o botão submeter
+  // chama a função que faz fetch para o backend
+
+  // Ler todos os campos do formulário e fazer Json.parse para criar um objeto
+  // Chamar a função sendNewProductReq com o objeto criado
 
   btn.onclick = function () {
-    modal.style.display = "block";
+    modal.style.display = 'block';
   };
 
   cancel.onclick = function () {
-    modal.style.display = "none";
+    modal.style.display = 'none';
     form.reset();
   };
 
   span.onclick = function () {
-    modal.style.display = "none";
+    modal.style.display = 'none';
     form.reset();
   };
 
   window.onclick = function (event) {
     if (event.target == modal) {
-      modal.style.display = "none";
+      modal.style.display = 'none';
       form.reset();
     }
   };
 }
 
 async function sendNewProductReq(product) {
-  const requestURL = productsPath;
-  const request = new Request(requestURL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(product),
-  });
-  const response = await fetch(request);
-  const result = await response.json();
-  return result;
+  try {
+    const requestURL = productsPath;
+    const response = await fetch(requestURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(product),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Erro ao enviar o produto: ${response.statusText}`);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error('Erro ao enviar o produto:', error);
+    throw error;
+  }
 }
