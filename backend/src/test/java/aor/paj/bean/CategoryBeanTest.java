33135d27545a4baeb5503fd767e8b90a83c87b28
Filipe Gomes <filipegomes@student.dei.uc.pt>
2025-03-06 17:49:32 +0000

    Testes: optimizacao de testes de conversao Dto/Entity e vice-versa

--- backend/src/test/java/aor/paj/bean/CategoryBeanTest.java
+++ backend/src/test/java/aor/paj/bean/CategoryBeanTest.java
@@ -13,44 +13,39 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CategoryBeanTest {
 
-    @InjectMocks // InjectMocks: Cria uma instância de CategoryBean e injeta os mocks marcados com @Mock
+    @InjectMocks // Cria uma instância de CategoryBean e injeta os mocks marcados com @Mock
     private CategoryBean categoryBean;
 
-    @Mock // Mock: Cria um mock para a dependência ProductDao
-    private ProductDao productDao;
-
-    @BeforeEach // BeforeEach: Este método é executado antes de cada teste
+    @BeforeEach // Executado antes de cada teste
     public void setUp() {
         MockitoAnnotations.openMocks(this); // Inicializa os mocks para este teste
     }
 
-    @Test // Test: Indica que este método é um caso de teste
+    @Test
     public void testConvertCategoryDtoToCategoryEntity() {
         // Preparar (Arrange): Configura os dados de entrada para o teste
         CategoryDto categoryDto = new CategoryDto();
         categoryDto.setId(1);
         categoryDto.setNome("Test Category");
 
-        // Agir (Act): Executa o método que está sendo testado
         CategoryEntity categoryEntity = categoryBean.convertCategoryDtoToCategoryEntity(categoryDto);
 
-        // Verificar (Assert): Verifica se o resultado é o esperado
+        // Verificação se o resultado é o esperado
         assertEquals(1L, categoryEntity.getId(), "O ID da categoria deve ser 1");
         assertEquals("Test Category", categoryEntity.getName(), "O nome da categoria deve ser 'Test Category'");
     }
 
-    @Test // Test: Indica que este método é um caso de teste
+    @Test
     public void testConvertCategoryEntityToCategoryDto() {
         // Preparar (Arrange): Configura os dados de entrada para o teste
         CategoryEntity categoryEntity = new CategoryEntity();
         categoryEntity.setId(2);
         categoryEntity.setName("Another Category");
 
-        // Agir (Act): Executa o método que está sendo testado
         CategoryDto categoryDto = categoryBean.convertCategoryEntityToCategoryDto(categoryEntity);
 
-        // Verificar (Assert): Verifica se o resultado é o esperado
-        assertEquals(2L, categoryDto.getId(), "O ID da categoria deve ser 2");
+        // Verificação se o resultado é o esperado
+        assertEquals(2, categoryDto.getId(), "O ID da categoria deve ser 2");
         assertEquals("Another Category", categoryDto.getNome(), "O nome da categoria deve ser 'Another Category'");
     }
 }
