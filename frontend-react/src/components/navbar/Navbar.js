e0d448b9c21d5d61ba18d94c5fed604cf67c1b3e
Filipe Gomes <filipegomes@student.dei.uc.pt>
2025-03-13 15:46:04 +0000

    Login: implementacao de logout (not working) Navbar insercao de icone de logout e respetiva formatacao

--- frontend-react/src/components/navbar/Navbar.js
+++ frontend-react/src/components/navbar/Navbar.js
@@ -2,8 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import StoreLogo from "../../images/icon.png";
+import { userStore } from "../../stores/UserStore";
+import { FaSignOutAlt } from "react-icons/fa";
 
 const Navbar = () => {
+  const username = userStore((state) => state.username); // Obtém o estado do username da store
+  const clearUser = userStore((state) => state.clearUser);
+
+  const handleLogout = async () => {
+    try {
+      const token = userStore.getState().token;
+      const response = await fetch(
+        "http://localhost:8080/filipe-proj4/rest/users/logout",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: token,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Logout failed");
+      }
+
+      clearUser(); // Limpa o estado do usuário na store
+      alert("Logout realizado com sucesso!");
+    } catch (error) {
+      console.error("Erro no logout:", error);
+      alert("Erro ao realizar logout. Tente novamente.");
+    }
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -16,11 +47,27 @@ const Navbar = () => {
               Home
             </Link>
           </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/login">
-              Login
-            </Link>
-          </li>
+          {!username && (
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
+          )}
+          {username && (
+            <li className="nav-item">
+              <Link className="nav-link" to="/profile">
+                Profile
+              </Link>
+            </li>
+          )}
+          {username && (
+            <li className="nav-item">
+              <button className="nav-link logout-button" onClick={handleLogout}>
+                <FaSignOutAlt />
+              </button>
+            </li>
+          )}
         </ul>
       </div>
     </nav>
