c18d2e8debdd6128c0e0ab38c87439e6a6a45dc6
Filipe Gomes <filipegomes@student.dei.uc.pt>
2025-04-21 18:11:29 +0100

    Perfis frontend: bugfixing na apresentação dos users resolvido

--- frontend-react/src/pages/UserList.js
+++ frontend-react/src/pages/UserList.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import api from "../services/apiService";
 import "./UserList.css";
 
 function UserList() {
   const [users, setUsers] = useState([]);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -24,18 +25,55 @@ function UserList() {
     return <div className="error">{error}</div>;
   }
 
+  const handleCardClick = (username) => {
+    navigate(`/profile/${username}`);
+  };
+
   return (
     <div className="user-list-container">
       <h1>Lista de Utilizadores</h1>
-      <ul>
+      <div className="user-list-cards-container">
         {users.map((user) => (
-          <li key={user.username}>
-            <Link to={`/profile/${user.username}`}>
-              {user.firstName} {user.lastName} ({user.username})
-            </Link>
-          </li>
+          <div
+            className="user-card"
+            key={user.username}
+            onClick={() => handleCardClick(user.username)}
+          >
+            {/* Primeira coluna: Imagem do utilizador */}
+            <div className="user-imagem">
+              <img
+                src={user.imagem || "https://via.placeholder.com/50"}
+                alt={user.username}
+                className="user-card-image"
+              />
+            </div>
+
+            {/* Segunda coluna: Informações do utilizador */}
+            <div className="user-card-info">
+              <span className="user-card-username">
+                <strong>Username:</strong> {user.username}
+              </span>
+              <span>
+                <strong>Nome completo:</strong> {user.firstName} {user.lastName}
+              </span>
+              <span>
+                <strong>Email:</strong> {user.email}
+              </span>
+              <span>
+                <strong>Tipo de user:</strong>{" "}
+                {user.isAdmin ? "Admin" : "Utilizador"}
+              </span>
+              <span
+                className={`user-card-status ${
+                  user.isDeleted ? "deleted" : "active"
+                }`}
+              >
+                <strong>Ativo:</strong> {user.isDeleted ? "Não" : "Sim"}
+              </span>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
